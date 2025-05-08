ca1081e6d6994c3fea12a755d6f9dc86b007e72b
Baron Chong <baronchong23@gmail.com>
2025-05-08 20:08:26 +0800

    Update Redis configuration with Lettuce pooling and client resources

--- src/main/java/com/d9/seckill/config/RedisConfig.java
+++ src/main/java/com/d9/seckill/config/RedisConfig.java
@@ -4,16 +4,49 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.*;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.resource.ClientResources;
+import io.lettuce.core.resource.DefaultClientResources;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
 
+    @Bean(destroyMethod = "shutdown")
+    public ClientResources clientResources() {
+        return DefaultClientResources.builder()
+                .ioThreadPoolSize(4)
+                .computationThreadPoolSize(4)
+                .build();
+    }
+
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379); // 可换成配置值
+    public RedisConnectionFactory redisConnectionFactory(ClientResources clientResources) {
+        GenericObjectPoolConfig<?> poolConfig = new GenericObjectPoolConfig<>();
+        poolConfig.setMaxTotal(8);
+        poolConfig.setMaxIdle(8);
+        poolConfig.setMinIdle(0);
+        poolConfig.setMaxWait(Duration.ofMillis(1000));
+
+        LettucePoolingClientConfiguration clientConfig = LettucePoolingClientConfiguration.builder()
+                .clientResources(clientResources)
+                .clientOptions(ClientOptions.builder()
+                        .autoReconnect(true)
+                        .disconnectedBehavior(ClientOptions.DisconnectedBehavior.REJECT_COMMANDS)
+                        .build())
+                .commandTimeout(Duration.ofSeconds(5))
+                .build();
+
+        RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration();
+        serverConfig.setHostName("localhost");
+        serverConfig.setPort(6379);
+
+        return new LettuceConnectionFactory(serverConfig, clientConfig);
     }
 
     @Bean
@@ -21,14 +54,12 @@ public class RedisConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
         
-        // 使用 StringRedisSerializer 來序列化和反序列化 redis 的 key
+        // Use StringRedisSerializer for keys
         template.setKeySerializer(new StringRedisSerializer());
-        // 使用 GenericJackson2JsonRedisSerializer 來序列化和反序列化 redis 的 value
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        
-        // Hash 的 key 也採用 StringRedisSerializer 的序列化方式
         template.setHashKeySerializer(new StringRedisSerializer());
-        // Hash 的 value 也採用 GenericJackson2JsonRedisSerializer 的序列化方式
+        
+        // Use GenericJackson2JsonRedisSerializer for values
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         
         template.afterPropertiesSet();
