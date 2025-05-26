2eb298c530236c950b70365eb17238637af7c9b9
Baron Chong <baronchong23@gmail.com>
2025-05-26 23:33:31 +0800

    feat: add H2 database dependency and test profile configuration

--- src/test/java/com/d9/seckill/SeckillApplicationTests.java
+++ src/test/java/com/d9/seckill/SeckillApplicationTests.java
@@ -2,8 +2,10 @@ package com.d9.seckill;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // 指定加载 application-test.yml
 class SeckillApplicationTests {
 
 	@Test
